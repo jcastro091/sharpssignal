67fe53b5ff69166c8295744c739f4d8a8bd6a920
JCastro091 <Jcastro091@aol.com>
2025-06-27 15:43:39 -0400

    🔧 Add GA4 snippet to _app, cleanup exports

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,39 @@
+// pages/_app.js
 import "../styles/globals.css";
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import Script from "next/script";
 
+export default function MyApp({ Component, pageProps }) {
+  const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
+  return (
+    <>
+      {/* Tailwind CDN (optional if using compiled CSS) */}
+      <Script
+        src="https://cdn.tailwindcss.com"
+        strategy="beforeInteractive"
+      />
+
+      {/* Global GA4 snippet */}
+      {gaId && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+            strategy="afterInteractive"
+          />
+          <Script id="ga4-config" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gaId}', {
+                page_path: window.location.pathname
+              });
+            `}
+          </Script>
+        </>
+      )}
+
+      <Component {...pageProps} />
+    </>
+  );
+}
